d3f9c1a0b92bcb9ebbc2940bed9190f223bf1ee8
Yonas <yone920@gmail.com>
2019-10-04 18:24:24 -0400

    todo controller now creating instances

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,17 @@ class CategoriesController < ApplicationController
         categories = Category.all
         render json: categories
     end
+
+    def create 
+        # byebug
+        category = Category.create(category_params)
+        categories = Category.all
+        render json: categories
+
+    end
+
+    private 
+    def category_params
+        params.require(:category).permit(:name)
+    end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,12 @@
 class TodosController < ApplicationController
+    def create 
+        todo = Todo.create(todo_params)
+        categories = Category.all
+        render json: categories
+    end
+
+    private 
+    def todo_params
+        params.require(:todo).permit(:text, :category_id)
+    end
 end
